bb01d770517cbcc94a8b05972c6510c30a378b00
M1kEk1M <M1kEk1M@rambler.ru>
2015-10-13 22:41:29 +0300

    Generator

--- game/scripts/mer_metaperson.py
+++ game/scripts/mer_metaperson.py
@@ -2,117 +2,144 @@ import random
 import renpy.store as store
 import renpy.exports as renpy
 from mer_person import Person 
-import inspect
 
 
+metapersonsdict = {'generic':{}, 'unique':{}}
+def check_script(metaname):
+    for f in renpy.list_files():
+        if f.startswith("characters") and 'script' in f and metaname in f:
+            holder = f.split('/')
+            return holder[len(holder)-1]
+    return None
+
+def init_metapersons_dict():
+    for f in renpy.list_files():
+        if f.startswith("characters/generic") and "avatar" in f:
+            key = f.split("/")[6]
+            color = f.split("/")[5]
+            age = f.split("/")[4]
+            gender = f.split("/")[3]
+            morphology = f.split("/")[2]
+            script = check_script(key)
+            if key in metapersonsdict['generic']:
+                metapersonsdict['generic'][key]['avatars'].append(f)
+            else:
+                metapersonsdict['generic'][key] = {
+                'morphology':morphology,
+                'gender':gender, 
+                'age':age, 
+                'color':color,
+                'avatars':[f], 
+                'script':script
+                }
+        if f.startswith("characters/unique") and "avatar" in f:
+            key = f.split("/")[6]
+            color = f.split("/")[5]
+            age = f.split("/")[4]
+            gender = f.split("/")[3]
+            morphology = f.split("/")[2]
+            script = check_script(key)
+            metapersonsdict['unique'][key] = {
+                'morphology':morphology,
+                'gender':gender, 
+                'age':age, 
+                'color':color,
+                'avatars':[f], 
+                'script':script,
+                'allready_used': False
+                }
+    return
+
+
+init_metapersons_dict()
+class GenPerson(Person):
+    def __init__(self, metaname=None, frequency="generic", *args, **kwargs):
+        self.frequency = frequency
+        self.metaname = metaname
+        self.meta_person = self.get_metaperson()
+        super(GenPerson, self).__init__()
+        self.color = self.get_parameter('color')
+        self.gender = self.get_parameter('gender')
+        self.age = self.get_parameter('age')
+        self.morphology = self.get_parameter('morphology')
+        self.meta_code = self.get_parameter("script")
+        self.avatar = self.get_parameter("avatars")
+        if self.meta_code:
+            self.metastats = self.call_script()
+        else:
+            self.metastats = None
+        if self.metastats:
+            for key in self.metastats.__dict__:
+                self.__dict__[key] = self.metastats.__dict__[key]
+
+    def get_metaperson(self):
+        if self.metaname:
+            return self.metaname
+        return random.choice(metapersonsdict[self.frequency].keys())
+
+    def get_parameter(self, param):
+        if param == "avatars":
+            return random.choice(metapersonsdict[self.frequency][self.meta_person][param])        
+        return metapersonsdict[self.frequency][self.meta_person][param]
+    def call_script(self):
+        sub_list = []
+        for sub in MetaPerson.__subclasses__():
+            if sub.meta_code in self.meta_code:
+                sub_list.append(sub)
+            if len(sub_list) > 0:
+                return random.choice(sub_list)()
+            else:
+                return None
+class GenPersonByGender(GenPerson):
+    def __init__(self, gender, *args, **kwargs):
+        self.gender = gender
+        super(GenPersonByGender, self).__init__()
+    def get_metaperson(self):
+        l = []
+        for meta in metapersonsdict[self.frequency].keys():
+            if metapersonsdict[self.frequency][meta]['gender'] == self.gender:
+                l.append(meta)
+        if len(l) > 0:
+            return random.choice(l)
+        raise Exception("No persons with such gender, %s"%(self.gender))
+
+
+class GenUnique(GenPerson):
+    def __init__(self, metaname=None, frequency="unique", *args, **kwargs):
+        self.frequency = 'unique'
+        super(GenUnique, self).__init__(metaname, frequency)
+    def get_metaperson(self):
+        uniques = metapersonsdict[self.frequency].keys()
+        if self.metaname and not metapersonsdict[self.frequency][self.metaname]['allready_used']:
+            return self.metaname
+        l = []
+        for meta in uniques:
+            used = metapersonsdict[self.frequency][meta]['allready_used']
+            if not used:
+                l.append(meta)
+                metapersonsdict[self.frequency][meta]['allready_used'] = True
+        if len(l) > 0:
+            return random.choice(l)
+        self.frequency = 'generic'
+        return random.choice(metapersonsdict['generic'].keys())
+    def call_script(self):
+        sub_list = []
+        for sub in UniqueMetaPerson.__subclasses__():
+            if sub.meta_code in self.meta_code:
+                sub_list.append(sub)
+            if len(sub_list) > 0:
+                return random.choice(sub_list)()
+            else:
+                return None
 
 class MetaPerson(Person):#all metapersons should inherit from this
-    meta_code = ""
+    meta_code = "NotImplemented"
     def __init__(self):
         super(MetaPerson, self).__init__()
 
 
 class UniqueMetaPerson(MetaPerson):#all unique metapersons should inherit from this
-    not_used = True
     def __init__(self):
         super(MetaPerson, self).__init__()
 
 
-
-class GenPerson(Person):#all character generators should inherit from this
-    def __init__(self):
-        super(GenPerson, self).__init__()
-        self.avatar = self.get_avatar()#path to metaperson's avatar
-        if not self.avatar or len(self.avatar.split("/")) != 9:#we wait that path starts with "characters/generic or unique/morphology/gender/age/color"
-            self.avatar = "characters/none.jpg"
-            return
-        self.meta_path = self.get_meta_path()#path to a folder containing this metaperson
-        self.meta_code = self.get_script()#name of this metaperson's script file     
-        self.meta_person = self.call_script()
-        self.color = self.meta_path.split('/')[5]
-        if self.meta_path.split('/')[4] in ("child", "young", "adult", "elder"):
-            self.age = self.meta_path.split('/')[4]
-        if self.meta_path.split('/')[3] in ("male", "female", "shemale", "sexless"):
-            self.gender = self.meta_path.split('/')[3]
-        self.species = self.meta_path.split('/')[2]
-        if self.meta_person:
-            for key in self.meta_person.__dict__:
-                self.__dict__[key] = self.meta_person.__dict__[key]
-            self.meta_person = None
-
-
-
-
-
-
-    def get_metapersons(self): #creates list of available metapersons
-        files = [f for f in renpy.list_files() if f.startswith("characters/generic") and "avatar" in f]
-        return files
-
-    def get_avatar(self):
-        available = self.get_metapersons()
-        if len(available) > 0:
-            return random.choice(available)
-        return None
-
-    def get_meta_path(self):#get path to a metaperson
-        meta_holder = self.avatar.split('/')
-        meta_holder.pop()
-        meta_holder.remove('avatar')
-        meta_path = ""
-        for name in meta_holder:
-            meta_path += "%s/"%(name)
-        return meta_path
-
-    def get_script(self):
-        for f in renpy.list_files():
-            if f.startswith("%sscript"%(self.meta_path)):
-                script = f.split('/').pop()
-                return script
-        return None
-
-    def call_script(self):
-        if self.meta_code:
-            sub_list = []
-            for sub in MetaPerson.__subclasses__():
-                if sub.meta_code in self.meta_code:
-                    sub_list.append(sub)
-            if len(sub_list) > 0:
-                return random.choice(sub_list)()
-        else:
-            return None
-
-class GenPersonByGender(GenPerson):
-    def __init__(self, gender):
-        self.search_gender = gender
-        super(GenPersonByGender, self).__init__()
-    def get_metapersons(self):
-        files = [f for f in renpy.list_files() if f.startswith("characters/generic") and "avatar" in f and self.search_gender in f.split('/')]
-        return files
-
-
-
-class GenUnique(GenPerson):
-    def __init__(self):
-        super(GenUnique, self).__init__()
-    def get_metapersons(self):
-        files = [f for f in renpy.list_files() if f.startswith("characters/unique") and "avatar" in f]
-        return files
-    def call_script(self):
-        if self.meta_code:
-            sub_list = []
-            for sub in UniqueMetaPerson.__subclasses__():
-                if sub.meta_code in self.meta_code and sub.not_used:
-                    sub.not_used = False
-                    sub_list.append(sub)
-            if len(sub_list) > 0:
-                return random.choice(sub_list)()
-        else:
-            return None
-class GenUniqueById(GenUnique):
-    def __init__(self, uniqueid="None"):
-        self.uniqueid = uniqueid
-        super(GenUniqueById, self).__init__()
-    def get_metapersons(self):
-        files = [f for f in renpy.list_files() if f.startswith("characters/unique") and "avatar" in f and self.uniqueid in f]
-        return files
